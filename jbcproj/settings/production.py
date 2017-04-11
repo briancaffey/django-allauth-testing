1b147858057d639548a8e0e4af6c0f726655d53b
Brian Caffey <briancaffey2010@gmail.com>
2017-04-11 17:38:14 -0400

    added email creds to production.py

--- jbcproj/settings/production.py
+++ jbcproj/settings/production.py
@@ -142,6 +142,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+#EMAIL
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'briancaffey2010@gmail.com'
+EMAIL_HOST_PASSWORD = 'saintmary'
+EMAIL_PORT = 587
+
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
