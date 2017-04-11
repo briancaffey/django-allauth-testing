19a4db7bdf8daf9cc981b31a20d3d5e2145d7a00
Brian Caffey <briancaffey2010@gmail.com>
2017-04-11 00:17:57 -0400

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jbcproj.wsgi --log-file -
--- jbcproj/settings/production.py
+++ jbcproj/settings/production.py
@@ -80,6 +80,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].updated(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
