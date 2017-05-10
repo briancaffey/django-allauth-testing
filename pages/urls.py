f99d13ea0256a8ea0f0eb0d34961aac7823e002d
Brian Caffey <briancaffey2010@gmail.com>
2017-05-10 02:54:24 -0400

    figured out how to display user data and list the users friends who are also using the app

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import HomeView
+from .views import HomeView, profile_view
 
 urlpatterns = [
 
-    url(r'^$', HomeView.as_view(), name="home")
+    url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^profile/$', profile_view, name="profile_view"),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from allauth.socialaccount.models import SocialAccount
 # Create your views here.
-
+import requests
 from django.views.generic import View
 # Create your views here.
 
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, 'pages/home.html', {})
-        
+
+def profile_view(request):
+	fb_uid = SocialAccount.objects.filter(user_id=request.user.id, provider='facebook')
+	fb_uid = fb_uid[0].uid
+	context = {}
+	if request.user.is_authenticated():
+
+		 target	= requests.get("https://graph.facebook.com/v2.9/" + fb_uid + "/friends?access_token=EAACaPmdoqowBAHa63DkZCDPFD8G0JZC0sA0ZBUoQB2Bi0f8mj3MX3lj30Y9Sie5Bk1oKE0wzMFgoFdXVr1h2NrCfoqYMzmZCsdXwDiJcQXinARafpwFuRiDsszyFDvdlPbCD5kqIPZB92RY01GBFWOSxnUv4fwlRNjccj4WBCyypcyQxuEJ8d")
+		 target = target.json()
+		 context['target'] = target
+	return render(request, 'pages/profile_view.html', context)
