4382adb687cfeae82972980a7ccdc5e9443fd332
Brian Caffey <briancaffey2010@gmail.com>
2017-05-10 13:39:52 -0400

    changed view to get social authentication tolkens from django-allauth

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.models import SocialToken
+
 # Create your views here.
 import requests
 from django.views.generic import View
@@ -14,8 +16,9 @@ def profile_view(request):
 	fb_uid = fb_uid[0].uid
 	context = {}
 	if request.user.is_authenticated():
-
-		 target	= requests.get("https://graph.facebook.com/v2.9/" + fb_uid + "/friends?access_token=EAACaPmdoqowBAHa63DkZCDPFD8G0JZC0sA0ZBUoQB2Bi0f8mj3MX3lj30Y9Sie5Bk1oKE0wzMFgoFdXVr1h2NrCfoqYMzmZCsdXwDiJcQXinARafpwFuRiDsszyFDvdlPbCD5kqIPZB92RY01GBFWOSxnUv4fwlRNjccj4WBCyypcyQxuEJ8d")
-		 target = target.json()
-		 context['target'] = target
+		tolken = SocialToken.objects.filter(account__user=request.user, account__provider='facebook').first()
+		print(tolken)
+		target	= requests.get("https://graph.facebook.com/v2.9/" + fb_uid + "/friends?access_token=" + str(tolken))
+		target = target.json()
+		context['target'] = target
 	return render(request, 'pages/profile_view.html', context)
